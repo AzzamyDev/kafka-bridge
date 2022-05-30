e8d4be24042e2fae00e571121f295899ba23a0d4
Angga Baskara <anggabaskara60@gmail.com>
2022-05-30 20:37:39 +0700

    #

--- src/main/java/com/azzmydev/bridge/MqttBean.java
+++ src/main/java/com/azzmydev/bridge/MqttBean.java
@@ -19,20 +19,27 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
 
+
 @Configuration
 public class MqttBean {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    private final static String TOPIC = "ais_kapal";
+    public final String AIS_KAPAL = "ais_kapal";
+    public final String RADAR_DATA = "radar_data";
+    public final String RADAR_IMAGE = "radar_image";
+    public final String TRACKING_DATA = "tracking_data";
+    public final String PERSONIL_IMAGE = "personil_image";
+    public final String PERSONIL_VIDEO = "personil_video";
+    public final String PERSONIL_GPS = "personil_gps";
 
     //MQTT ClientFactory
     public MqttPahoClientFactory mqttPahoClientFactory(){
         DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
         MqttConnectOptions options = new MqttConnectOptions();
 
-        options.setServerURIs(new String[] {"tcp://192.168.18.205"});
+        options.setServerURIs(new String[] {"tcp://localhost"});
         options.setCleanSession(true);
         factory.setConnectionOptions(options);
 
@@ -60,14 +67,43 @@ public class MqttBean {
     @ServiceActivator(inputChannel = "mqttInputChannel")
     public MessageHandler handler(){
         return new MessageHandler() {
+
             @Override
             public void handleMessage(Message<?> message) throws MessagingException {
                 String topic = message.getHeaders().get(MqttHeaders.RECEIVED_TOPIC).toString();
-                if (topic.equals(TOPIC)){
-                    System.out.println("This is our topic");
-                    System.out.println(message.getPayload());
-                    kafkaTemplate.send(TOPIC, message.getPayload().toString());
-                }
+                System.out.println(topic);
+
+                switch (topic){
+                   case AIS_KAPAL :{
+                    kafkaTemplate.send(AIS_KAPAL, message.getPayload().toString());
+                       break;
+                   }
+                   case RADAR_DATA :{
+                    kafkaTemplate.send(RADAR_DATA, message.getPayload().toString());
+                       break;
+                   }
+                   case RADAR_IMAGE :{
+                    kafkaTemplate.send(RADAR_IMAGE, message.getPayload().toString());
+                       break;
+                   }
+                   case TRACKING_DATA :{
+                    kafkaTemplate.send(TRACKING_DATA, message.getPayload().toString());
+                       break;
+                   }
+                   case PERSONIL_IMAGE :{
+                    kafkaTemplate.send(PERSONIL_IMAGE, message.getPayload().toString());
+                       break;
+                   }
+                   case PERSONIL_VIDEO :{
+                    kafkaTemplate.send(PERSONIL_VIDEO, message.getPayload().toString());
+                       break;
+                   }
+                   case PERSONIL_GPS :{
+                    kafkaTemplate.send(PERSONIL_GPS, message.getPayload().toString());
+                       break;
+                   }
+                   default:
+               }
             }
         };
     }
